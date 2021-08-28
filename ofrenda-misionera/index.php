a51c1309c3a79c241395c919bda0512a6e26eb18
nilsonuzcategui <nilsonuzcategui.19@gmail.com>
2021-08-28 13:10:28 -0400

    listo las ofrendas misioneras

--- ofrenda-misionera/index.php
+++ ofrenda-misionera/index.php
@@ -75,101 +75,47 @@
                         </div>
                         
                         <div class="col-md-6">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">
-                                Some quick example text to build on the card title and make up the bulk of the
-                                card's content.
-                                </p>
-                                <button type="button" class="btn btn-primary">Button</button>
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Desde Venezuela</h5>
+                                    <p class="card-text"><b>Banco de Venezuela</b></p>
+                                    <p class="card-text">Cuenta corriente Nº: <b>01020346590000052676</b></p>
+                                    <p class="card-text">A nombre de: <b>Obra Luz del Mundo de Venezuela</b></p>
+                                    <p class="card-text">RIF: J-08530470-3</p>
+                                    <p class="card-text">Dirección: 2 Calle, Barrio Colombia Norte. Casa No. 7-85, Guanare. Edo. Portuguesa. Venezuela.</p>
+                                </div>
+                                </div>
                             </div>
-                            </div>
-                        </div>
                         <div class="col-md-6">
-                            <!-- Carousel wrapper -->
-                                <div
-                                id="carruselNotiAlegria"
-                                class="carousel slide carousel-fade ms-3"
-                                data-mdb-ride="carousel"
-                                >
-
-                                <!-- Inner -->
-                                <div class="carousel-inner">
-                                    <!-- Single item -->
-                                    <div class="carousel-item active">
-                                    <img
-                                        src="<?=$ruta_inicio?>/img/notialegria/foto1.png"
-                                        class="d-block"
-                                    />
-                                    </div>
-
-                                    <div class="carousel-item">
-                                    <img
-                                        src="<?=$ruta_inicio?>/img/notialegria/foto2.png"
-                                        class="d-block"
-                                    />
-                                    </div>
-
-                                    <div class="carousel-item">
-                                    <img
-                                        src="<?=$ruta_inicio?>/img/notialegria/foto3.png"
-                                        class="d-block"
-                                    />
-                                    </div>
-
-                                    <div class="carousel-item">
-                                    <img
-                                        src="<?=$ruta_inicio?>/img/notialegria/foto4.png"
-                                        class="d-block"
-                                    />
-                                    </div>
-
-                                    <div class="carousel-item">
-                                    <img
-                                        src="<?=$ruta_inicio?>/img/notialegria/foto5.jpg"
-                                        class="d-block"
-                                    />
-                                    </div>
-                                    
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Desde Otros Paises</h5>
+                                    <p>
+                                        <b>Banco BBVA</b><br>
+                                        Cuenta Corriente: A nombre de: Iglesia Evangélica Luz Del Mundo De España.
+                                    </p>
+                                    <p><b>0182-1635-71-020-1512608</b></p>
+                                    <p>IBAN.ES09-0182-1635-7102-0151-2608</p>
+                                    <p>
+                                        SWIF. BBVAESMMXXX <br>
+                                        Tlf: 0257-2510786.
+                                    </p>
                                 </div>
-                                <!-- Inner -->
-
-                                <!-- Controls -->
-                                <button
-                                    class="carousel-control-prev"
-                                    type="button"
-                                    data-mdb-target="#carruselNotiAlegria"
-                                    data-mdb-slide="prev"
-                                >
-                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                    <span class="visually-hidden">Previous</span>
-                                </button>
-                                <button
-                                    class="carousel-control-next"
-                                    type="button"
-                                    data-mdb-target="#carruselNotiAlegria"
-                                    data-mdb-slide="next"
-                                >
-                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                    <span class="visually-hidden">Next</span>
-                                </button>
                                 </div>
-                                <!-- Carousel wrapper -->
-                        </div>
-
-
-                        <div class="col-12">
-                            <h2 class="titulo-fuente">Contácto</h2>
-                            <hr>
-                            <p>
-                                Si deseas ser partícipe de esta noble visión, escríbenos atraves de
-                            </p>
-                            <div>
-                                <a class="btn btn-success" href="https://api.whatsapp.com/send?phone=584145431445&text=Quiero%20contactar%20con%20la%20administraci%C3%B3n%20de%20notiluzinternacional" target="_blank">Whatsapp</a>
-                                <a class="btn btn-info" href="periodiconotiluzinternacional@gmail.com">Correo Electrónico</a>
                             </div>
                         </div>
+
+                        <div class="col-12 mb-3 mt-3">
+                            <img src="<?= $ruta_inicio ?>/img/comonaciolaofrendaopt.png" class="img-banner">
+                        </div>
+
+                        <div class="col-md-12">
+                        <div _ngcontent-serverapp-c94="" class="col-12 mt-4"><p _ngcontent-serverapp-c94=""> El Pastor Puertas nos recuerda el origen de la sabia decisión de implementar la Ofrenda Misionera para expandir el evangelio y para ganar almas para el reino de Dios.
+Toda la gran familia debe saber la importancia de ofrendar para la Obra del Señor y los resultados positivos obtenidos gracias a vuestro esfuerzo. </p><h3 class="titulo-fuente">El Dr. Puertas nos comenta:</h3><p _ngcontent-serverapp-c94=""><b _ngcontent-serverapp-c94="">LA PRIMERA REFERENCIA QUE SE TIENE DE LA OFRENDA MISIONERA EN LA BIBLIA APARECE EN:</b> Romanos 15:25-27 "...Mas ahora voy a Jerusalem para ministrar a los santos. Porque Macedonia y Acaya tuvieron a bien hacer una ofrenda para los pobres que hay entre los santos que están en Jerusalem. Pues les pareció bueno, y son deudores a ellos; porque si los gentiles han sido hechos participantes de sus bienes espirituales, deben también ellos ministrarles de los materiales." </p><p _ngcontent-serverapp-c94=""><b _ngcontent-serverapp-c94="">ENTRE NOSOTROS, LA "LUZ DEL MUNDO" SURGIÓ, CUANDO ME ENCONTRABA COMO UN BARCO VARADO EN MÁLAGA ESPAÑA:</b> Recibí un mensaje de un consiervo venezolano: Exhortándome fuertemente, diciéndome:
+- "Hno. Puertas, ¿Cuándo vas a seguir para Israel?, tu saliste hacia Tierra Santa, ¿ y estás anclado en E s p a ñ a...? </p><p _ngcontent-serverapp-c94=""><b _ngcontent-serverapp-c94="">LLEVÁBAMOS AÑO Y MEDIO PREDICANDO EN LAS PLAZAS, Y EN LAS ESQUINAS, SIN HABER GANADO NI UNA SOLA ALMA...</b> No habían ofrendas... comíamos pescado, que el hermano Edgar Arias rebuscaba de los pescadores, después de ayudarlos cada día, en el muelle a orilla del Mar Mediterráneo..! </p><p _ngcontent-serverapp-c94=""> COMO NO TENÍAMOS OVEJAS, YO ME IBA LOS DOMINGOS A CONGREGARME EN UNA IGLESIA BAUTISTA; AL LLEGAR UN DOMINGO A DICHA IGLESIA VI UNOS SOBRES Y TOMANDO UNO, LEÍ EN SU RÓTULO: "OFRENDA MISIONERA". ENTENDÍ QUE DIOS ME ESTABA MOSTRANDO EL CAMINO, PARA PODER LLEGAR A JERUSALEM </p><p _ngcontent-serverapp-c94=""> INMEDIATAMENTE ENVIÉ A NUESTRO HERMANO EDGAR ARIAS A AMÉRICA LATINA, PARA QUE VISITARA A LOS HERMANOS AMIGOS, EMPRESARIOS: </p><p _ngcontent-serverapp-c94=""> 1) EL LES EXPLICÓ SOBRE NUESTRA "NECESIDAD FINANCIERA" Y ASÍ NACIÓ "LA PRIMERA OFRENDA MISIONERA" </p><p _ngcontent-serverapp-c94=""> 2) REGRESÓ CON LOS PASAJES, Y ALGO DE DINERO, CON EL CUAL VOLAMOS A JERUSALEM..! </p><p _ngcontent-serverapp-c94=""> PERMITA DIOS, QUE EN TODAS LAS MISIONES: EN CADA PAÍS, CONTRIBUYAN CON LA OFRENDA MISIONERA; PORQUE POR MEDIO DE ELLA, HOY ESTAMOS EN LOS CINCO CONTINENTES... PERO LA OBRA NO HA TERMINADO..! OREN POR ESTO, PARA QUE LO HAGAN CON ALEGRÍA Y BUENA VOLUNTAD </p><p _ngcontent-serverapp-c94="">LES FELICITO POR TAN GRANDE INICIATIVA..!</p></div>
+                        </div>
+
+                        
                         
 
                     </div>
