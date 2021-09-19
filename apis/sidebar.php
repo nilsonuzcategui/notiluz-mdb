05a26722f3d68c7f78bd0fd4ab07690f9c85233d
nilsonuzcategui <nilsonuzcategui.19@gmail.com>
2021-09-19 10:21:57 -0400

    puliendo detalles

--- apis/sidebar.php
+++ apis/sidebar.php
@@ -1,56 +1,85 @@
 <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-          <div class="position-sticky pt-3">
-            <ul class="nav flex-column">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
-                  <span data-feather="home"></span>
-                  Inicio
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="user"></span>
-                  Hrno. Puertas
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="award"></span>
-                  Venezuela
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="globe"></span>
-                  Internacional
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="radio"></span>
-                  Radios
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="layers"></span>
-                  Notiluz
-                </a>
-              </li>
-            </ul>
+  
+  <div class="position-sticky pt-3">
+    <ul class="nav flex-column">
+      <li class="nav-item">
+        <a class="nav-link" aria-current="page" href="<?=$ruta_inicio?>">
+          <span data-feather="home"></span>
+          Inicio
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link dropdown" href="#" id="menuSidebarJaimePuertas" data-mdb-toggle="dropdown" aria-expanded="false">
+          <span data-feather="user"></span>
+          Hrno. Puertas
+        </a>
+        <ul class="dropdown-menu" aria-labelledby="menuSidebarJaimePuertas">
+          <li><a class="dropdown-item" href="#">Micros</a></li>
+          <li><a class="dropdown-item" href="<?=$ruta_inicio?>/tratados/">Tratados</a></li>
+          <li><a class="dropdown-item" href="<?=$ruta_inicio?>/preguntas-y-respuestas/">Preguntas y Respuestas</a></li>
+          <li><a class="dropdown-item" href="<?=$ruta_inicio?>/libros/">Libros</a></li>
+        </ul>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link dropdown" href="#" id="menuSidebarVenezuela" data-mdb-toggle="dropdown" aria-expanded="false">
+          <span data-feather="award"></span>
+          Venezuela
+        </a>
+        <ul class="dropdown-menu" aria-labelledby="menuSidebarVenezuela">
+          <li><a class="dropdown-item" href="#">Panes Calientes</a></li>
+          <li><a class="dropdown-item" href="#">Informativos</a></li>
+          <li><a class="dropdown-item" href="#">Radios</a></li>
+          <li><a class="dropdown-item" href="#">Guía Dominical</a></li>
+          <li><a class="dropdown-item" href="#">Campamentos</a></li>
+        </ul>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link dropdown" href="#" id="menuSidebarInternacional" data-mdb-toggle="dropdown" aria-expanded="false">
+          <span data-feather="globe"></span>
+          Internacional
+        </a>
+        <ul class="dropdown-menu" aria-labelledby="menuSidebarInternacional">
+          <li><a class="dropdown-item" href="#">Directorio</a></li>
+          <li><a class="dropdown-item" href="#">Noticias</a></li>
+        </ul>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link dropdown" href="#" id="menuSidebarRadios" data-mdb-toggle="dropdown" aria-expanded="false">
+          <span data-feather="radio"></span>
+          Radios
+        </a>
+        <ul class="dropdown-menu" aria-labelledby="menuSidebarRadios">
+          <li><a class="dropdown-item" href="#">Nacionales</a></li>
+          <li><a class="dropdown-item" href="#">Internaciolanes</a></li>
+        </ul>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link dropdown" href="#" id="menuSidebarNotiluz" data-mdb-toggle="dropdown" aria-expanded="false">
+          <span data-feather="layers"></span>
+          Notiluz
+        </a>
+        <ul class="dropdown-menu" aria-labelledby="menuSidebarRadios">
+          <li><a class="dropdown-item" href="<?=$ruta_inicio?>/noticias/">Noticias</a></li>
+          <li><a class="dropdown-item" href="<?=$ruta_inicio?>/notialegria/">Notialegria</a></li>
+          <li><a class="dropdown-item" href="<?=$ruta_inicio?>/agroecoluz/">Agroecoluz</a></li>
+          <li><a class="dropdown-item" href="#">Nosotros</a></li>
+          <li><a class="dropdown-item" href="#">Equipo</a></li>
+        </ul>
+      </li>
+    </ul>
 
-            <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-              <span>Otros Enlaces</span>
-              
-            </h6>
-            <ul class="nav flex-column mb-2">
-              <li class="nav-item">
-                <a class="nav-link" href="<?= $ruta_inicio ?>/ofrenda-misionera/">
-                  <span data-feather="heart"></span>
-                  Ofrenda Misionera
-                </a>
-              </li>
-              </ul>
-        
-          </div>
-        </nav>
+    <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+      <span>Otros Enlaces</span>
+
+    </h6>
+    <ul class="nav flex-column mb-2">
+      <li class="nav-item">
+        <a class="nav-link" href="<?= $ruta_inicio ?>/ofrenda-misionera/">
+          <span data-feather="heart"></span>
+          Ofrenda Misionera
+        </a>
+      </li>
+    </ul>
+
+  </div>
+</nav>
--- ofrenda-misionera/index.php
+++ ofrenda-misionera/index.php
@@ -2,14 +2,17 @@
 <html lang="en">
 
 <head>
-    <?php include_once('../apis/head.php') ?>
+    <?php
+    include_once('../apis/head.php');
+    $descriptionAux = 'La ofrenda misionera responde a toda una estructura y a todo un concepto de lo que son las misiones. No es una añadidura ni una carga para la Iglesia local. La ofrenda misionera es una gran bendición tanto para la obra como para quienes partimos de ella.';
+    ?>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
     <title>Ofrenda Misionera | Notiluz</title>
-    <meta name="description" content="Aquí encontrarás las  explícitas Noticias, de nuestra amada 'Obra Luz del Mundo' a nivel Nacional e Internacional y otros aconteceres del mundo cristiano, las mejores y más importantes acciones de nuestro día a día las podrás disfrutar aquí.">
-    <meta property="og:title" content="Notiluz | Noticias Luz del Mundo" />
-    <meta property="og:description" content="Aquí encontrarás las  explícitas Noticias, de nuestra amada 'Obra Luz del Mundo' a nivel Nacional e Internacional y otros aconteceres del mundo cristiano, las mejores y más importantes acciones de nuestro día a día las podrás disfrutar aquí." />
+    <meta name="description" content="<?=$descriptionAux?>">
+    <meta property="og:title" content="Ofrenda Misionera | Notiluz" />
+    <meta property="og:description" content="<?=$descriptionAux?>" />
     <meta property="og:url" content="hptts://notiluz.com/">
     <meta property="og:site_name" content="Notiluz" />
     <meta property="og:image" content="<?= $ruta_inicio ?>/img/logo-para-redes.png" />
@@ -32,32 +35,7 @@
             <?php include_once('../apis/sidebar.php') ?>
 
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-                <!-- SLIDER HOME -->
-                <div id="carouselExampleIndicators" class="carousel slide" data-mdb-ride="carousel">
-
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img src="<?= $ruta_inicio ?>/img/slidertop1.jpg" class="d-block w-100" />
-                        </div>
-                        <div class="carousel-item">
-                            <img src="<?= $ruta_inicio ?>/img/slidertop2.jpg" class="d-block w-100" />
-                        </div>
-                    </div>
-                    <button class="carousel-control-prev" type="button" data-mdb-target="#carouselExampleIndicators" data-mdb-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-mdb-target="#carouselExampleIndicators" data-mdb-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
-                </div>
-                <!-- LOGOS -->
-                <div style="display: flex;justify-content: space-around;">
-                    <img src="<?= $ruta_inicio ?>/img/logo.png" style="width: 120px;height: 120px;">
-                    <img src="<?= $ruta_inicio ?>/img/logo-luz-del-mundo.png" style="width: 120px;height: 120px;">
-                </div>
-                <hr>
+                <?php include_once('../apis/slider-logo-header.php') ?>
 
 
                 <div class="align-items-center pt-3 pb-2 mb-3">
@@ -68,12 +46,12 @@
                             <img src="<?= $ruta_inicio ?>/img/banner/banner-ofrenda-opt.jpg" class="img-banner">
                         </div>
 
-                        
+
                         <div class="col-md-12">
                             <h2 class="titulo-fuente">Ofrenda Misionera</h2>
                             <hr>
                         </div>
-                        
+
                         <div class="col-md-6">
                             <div class="card">
                                 <div class="card-body">
@@ -84,8 +62,8 @@
                                     <p class="card-text">RIF: J-08530470-3</p>
                                     <p class="card-text">Dirección: 2 Calle, Barrio Colombia Norte. Casa No. 7-85, Guanare. Edo. Portuguesa. Venezuela.</p>
                                 </div>
-                                </div>
                             </div>
+                        </div>
                         <div class="col-md-6">
                             <div class="card">
                                 <div class="card-body">
@@ -101,29 +79,41 @@
                                         Tlf: 0257-2510786.
                                     </p>
                                 </div>
-                                </div>
                             </div>
                         </div>
-
-                        <div class="col-12 mb-3 mt-3">
-                            <img src="<?= $ruta_inicio ?>/img/comonaciolaofrendaopt.png" class="img-banner">
-                        </div>
-
-                        <div class="col-md-12">
-                        <div _ngcontent-serverapp-c94="" class="col-12 mt-4"><p _ngcontent-serverapp-c94=""> El Pastor Puertas nos recuerda el origen de la sabia decisión de implementar la Ofrenda Misionera para expandir el evangelio y para ganar almas para el reino de Dios.
-Toda la gran familia debe saber la importancia de ofrendar para la Obra del Señor y los resultados positivos obtenidos gracias a vuestro esfuerzo. </p><h3 class="titulo-fuente">El Dr. Puertas nos comenta:</h3><p _ngcontent-serverapp-c94=""><b _ngcontent-serverapp-c94="">LA PRIMERA REFERENCIA QUE SE TIENE DE LA OFRENDA MISIONERA EN LA BIBLIA APARECE EN:</b> Romanos 15:25-27 "...Mas ahora voy a Jerusalem para ministrar a los santos. Porque Macedonia y Acaya tuvieron a bien hacer una ofrenda para los pobres que hay entre los santos que están en Jerusalem. Pues les pareció bueno, y son deudores a ellos; porque si los gentiles han sido hechos participantes de sus bienes espirituales, deben también ellos ministrarles de los materiales." </p><p _ngcontent-serverapp-c94=""><b _ngcontent-serverapp-c94="">ENTRE NOSOTROS, LA "LUZ DEL MUNDO" SURGIÓ, CUANDO ME ENCONTRABA COMO UN BARCO VARADO EN MÁLAGA ESPAÑA:</b> Recibí un mensaje de un consiervo venezolano: Exhortándome fuertemente, diciéndome:
-- "Hno. Puertas, ¿Cuándo vas a seguir para Israel?, tu saliste hacia Tierra Santa, ¿ y estás anclado en E s p a ñ a...? </p><p _ngcontent-serverapp-c94=""><b _ngcontent-serverapp-c94="">LLEVÁBAMOS AÑO Y MEDIO PREDICANDO EN LAS PLAZAS, Y EN LAS ESQUINAS, SIN HABER GANADO NI UNA SOLA ALMA...</b> No habían ofrendas... comíamos pescado, que el hermano Edgar Arias rebuscaba de los pescadores, después de ayudarlos cada día, en el muelle a orilla del Mar Mediterráneo..! </p><p _ngcontent-serverapp-c94=""> COMO NO TENÍAMOS OVEJAS, YO ME IBA LOS DOMINGOS A CONGREGARME EN UNA IGLESIA BAUTISTA; AL LLEGAR UN DOMINGO A DICHA IGLESIA VI UNOS SOBRES Y TOMANDO UNO, LEÍ EN SU RÓTULO: "OFRENDA MISIONERA". ENTENDÍ QUE DIOS ME ESTABA MOSTRANDO EL CAMINO, PARA PODER LLEGAR A JERUSALEM </p><p _ngcontent-serverapp-c94=""> INMEDIATAMENTE ENVIÉ A NUESTRO HERMANO EDGAR ARIAS A AMÉRICA LATINA, PARA QUE VISITARA A LOS HERMANOS AMIGOS, EMPRESARIOS: </p><p _ngcontent-serverapp-c94=""> 1) EL LES EXPLICÓ SOBRE NUESTRA "NECESIDAD FINANCIERA" Y ASÍ NACIÓ "LA PRIMERA OFRENDA MISIONERA" </p><p _ngcontent-serverapp-c94=""> 2) REGRESÓ CON LOS PASAJES, Y ALGO DE DINERO, CON EL CUAL VOLAMOS A JERUSALEM..! </p><p _ngcontent-serverapp-c94=""> PERMITA DIOS, QUE EN TODAS LAS MISIONES: EN CADA PAÍS, CONTRIBUYAN CON LA OFRENDA MISIONERA; PORQUE POR MEDIO DE ELLA, HOY ESTAMOS EN LOS CINCO CONTINENTES... PERO LA OBRA NO HA TERMINADO..! OREN POR ESTO, PARA QUE LO HAGAN CON ALEGRÍA Y BUENA VOLUNTAD </p><p _ngcontent-serverapp-c94="">LES FELICITO POR TAN GRANDE INICIATIVA..!</p></div>
-                        </div>
-
-                        
-                        
-
                     </div>
 
+                    <div class="col-12 mb-3 mt-3">
+                        <img src="<?= $ruta_inicio ?>/img/comonaciolaofrendaopt.png" class="img-banner">
+                    </div>
+
+                    <div class="col-md-12">
+                        <div _ngcontent-serverapp-c94="" class="col-12 mt-4">
+                            <p _ngcontent-serverapp-c94=""> El Pastor Puertas nos recuerda el origen de la sabia decisión de implementar la Ofrenda Misionera para expandir el evangelio y para ganar almas para el reino de Dios.
+                                Toda la gran familia debe saber la importancia de ofrendar para la Obra del Señor y los resultados positivos obtenidos gracias a vuestro esfuerzo. </p>
+                            <h3 class="titulo-fuente">El Dr. Puertas nos comenta:</h3>
+                            <p _ngcontent-serverapp-c94=""><b _ngcontent-serverapp-c94="">LA PRIMERA REFERENCIA QUE SE TIENE DE LA OFRENDA MISIONERA EN LA BIBLIA APARECE EN:</b> Romanos 15:25-27 "...Mas ahora voy a Jerusalem para ministrar a los santos. Porque Macedonia y Acaya tuvieron a bien hacer una ofrenda para los pobres que hay entre los santos que están en Jerusalem. Pues les pareció bueno, y son deudores a ellos; porque si los gentiles han sido hechos participantes de sus bienes espirituales, deben también ellos ministrarles de los materiales." </p>
+                            <p _ngcontent-serverapp-c94=""><b _ngcontent-serverapp-c94="">ENTRE NOSOTROS, LA "LUZ DEL MUNDO" SURGIÓ, CUANDO ME ENCONTRABA COMO UN BARCO VARADO EN MÁLAGA ESPAÑA:</b> Recibí un mensaje de un consiervo venezolano: Exhortándome fuertemente, diciéndome:
+                                - "Hno. Puertas, ¿Cuándo vas a seguir para Israel?, tu saliste hacia Tierra Santa, ¿ y estás anclado en E s p a ñ a...? </p>
+                            <p _ngcontent-serverapp-c94=""><b _ngcontent-serverapp-c94="">LLEVÁBAMOS AÑO Y MEDIO PREDICANDO EN LAS PLAZAS, Y EN LAS ESQUINAS, SIN HABER GANADO NI UNA SOLA ALMA...</b> No habían ofrendas... comíamos pescado, que el hermano Edgar Arias rebuscaba de los pescadores, después de ayudarlos cada día, en el muelle a orilla del Mar Mediterráneo..! </p>
+                            <p _ngcontent-serverapp-c94=""> COMO NO TENÍAMOS OVEJAS, YO ME IBA LOS DOMINGOS A CONGREGARME EN UNA IGLESIA BAUTISTA; AL LLEGAR UN DOMINGO A DICHA IGLESIA VI UNOS SOBRES Y TOMANDO UNO, LEÍ EN SU RÓTULO: "OFRENDA MISIONERA". ENTENDÍ QUE DIOS ME ESTABA MOSTRANDO EL CAMINO, PARA PODER LLEGAR A JERUSALEM </p>
+                            <p _ngcontent-serverapp-c94=""> INMEDIATAMENTE ENVIÉ A NUESTRO HERMANO EDGAR ARIAS A AMÉRICA LATINA, PARA QUE VISITARA A LOS HERMANOS AMIGOS, EMPRESARIOS: </p>
+                            <p _ngcontent-serverapp-c94=""> 1) EL LES EXPLICÓ SOBRE NUESTRA "NECESIDAD FINANCIERA" Y ASÍ NACIÓ "LA PRIMERA OFRENDA MISIONERA" </p>
+                            <p _ngcontent-serverapp-c94=""> 2) REGRESÓ CON LOS PASAJES, Y ALGO DE DINERO, CON EL CUAL VOLAMOS A JERUSALEM..! </p>
+                            <p _ngcontent-serverapp-c94=""> PERMITA DIOS, QUE EN TODAS LAS MISIONES: EN CADA PAÍS, CONTRIBUYAN CON LA OFRENDA MISIONERA; PORQUE POR MEDIO DE ELLA, HOY ESTAMOS EN LOS CINCO CONTINENTES... PERO LA OBRA NO HA TERMINADO..! OREN POR ESTO, PARA QUE LO HAGAN CON ALEGRÍA Y BUENA VOLUNTAD </p>
+                            <p _ngcontent-serverapp-c94="">LES FELICITO POR TAN GRANDE INICIATIVA..!</p>
+                        </div>
+                    </div>
+
+
+
+
                 </div>
-            </main>
 
         </div>
+        </main>
+
+    </div>
     </div>
 
 
